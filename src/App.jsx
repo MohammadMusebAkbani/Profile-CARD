26c014d05fd78ea3350099de061342e9e93bfb0e
MohammadMusebAkbani <akbanimuseb@gmail.com>
2025-09-29 13:32:54 +0530

    remove dublicate code

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     "https://img.freepik.com/premium-photo/studio-portrait-handsome-young-man-astonished-with-something_1048944-17590200.jpg?semt=ais_hybrid&w=740&q=80";
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="flex flex-col w-full max-w-[50rem] h-[30rem] items-center justify-center mx-auto bg-gray-400 rounded-3xl">
+      <div className="flex w-full max-w-[50rem] h-[30rem] items-center justify-center mx-auto bg-gray-400 rounded-3xl">
         <div className="flex flex-col h-[400px] w-[300px] bg-white rounded-lg shadow-lg relative overflow-hidden">
           {/* Blue header section */}
           <div className="bg-blue-500 h-1/3 rounded-t-lg"></div>
@@ -17,15 +17,14 @@ function App() {
           <img
             src={imageUrl}
             alt="Profile"
-            className="absolute top-[6rem] left-1/2 -translate-x-1/2 -translate-y-1/2 w-32 h-32 rounded-full border-[4px] border-blue-500 object-cover shadow-md bg-amber-50"
+            className="absolute top-[6rem] left-1/2 -translate-x-1/2 -translate-y-1/2 w-32 h-32 rounded-full border-[4px] border-blue-500 object-cover shadow-md"
           />
           {/* Content section */}
-          <div className="flex flex-col items-center flex-1 pt-10 gap-1">
+          <div className="flex flex-col items-center mt-10 gap-1">
             <h1 className="text-center font-bold text-2xl text-gray-800">
               CodingLab
             </h1>
             <p>Youtuber & Blogger</p>
-
             <div className="flex gap-3 mt-3">
               <div className="h-8 w-8 rounded-full bg-blue-700 flex items-center justify-center">
                 <RiFacebookFill className="text-white font-bold" />
